aeb0595fc76213c3b7805b695fa46074f25746a0
Jildert Venema <jildert.venema@hotmail.com>
2021-05-15 09:52:43 +0200

    Initial Commit

---
+++ Pixel Finder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pixel Finder
+//
+//  Created by Jildert Venema on 15/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pixel Finder/Pixel_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pixel_FinderApp.swift
+//  Pixel Finder
+//
+//  Created by Jildert Venema on 15/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Pixel_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
